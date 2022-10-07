9ffc0a8e35d49def1114e7381f7116720de0716d
j-eduardosandoval <eduardosandoval.atc@gmail.com>
2022-10-07 14:48:31 -0500

    Predeploy

--- src/components/HomeIam.jsx
+++ src/components/HomeIam.jsx
@@ -7,7 +7,7 @@ const HomeIam = () => {
         <section className="home" id="home">
 <div className="home__container bd-container bd-grid">
     <div className="home__data">
-        <h1 className="home__title">eduardSando</h1>
+        <h1 className="home__title">eduardo</h1>
         <h2 className="home__subtitle">Soy un development <br /> in process</h2>
         <a href="/" className="button">Mas sobre mi</a>
     </div>
